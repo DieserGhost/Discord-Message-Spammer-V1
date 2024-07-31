d126814219bec7c5eaad85f7858dd84355f70ba0
DieserGhost <144170441+DieserGhost@users.noreply.github.com>
2024-07-31 09:30:29 +0200

    Update Discord_Message_Spammer_V1.py

--- Discord-Message-Spammer-V1/Discord_Message_Spammer_V1.py
+++ Discord-Message-Spammer-V1/Discord_Message_Spammer_V1.py
@@ -1,9 +1,10 @@
 import json
 import requests
+import time
 
+print('DiscordMessageSpammer V1 | Sending your Message...')
 
-print('DiscordMessageSpammer V1| Sending your Message...')
-
+# Load configuration from config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
@@ -11,10 +12,20 @@ payload = {
     'content': config["message_content"]
 }
 
-
 header = {
-  'authorization': config["authorization_key"]  
+    'authorization': config["authorization_key"]
 }
 
-for i in range(10000):
-    r = requests.post(config["api_url"], data=payload, headers=header)
+# Function to make 25 POST requests
+def make_requests():
+    for _ in range(25):
+        try:
+            response = requests.post(config["api_url"], data=payload, headers=header)
+            print(f"Status Code: {response.status_code}, Response: {response.text}")
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+
+# Loop for the total number of iterations
+for _ in range(10000):
+    make_requests()
+    time.sleep(5)
